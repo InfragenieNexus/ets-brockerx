4a62f5b500ea9784864b156cffcbd55620a1b3d4
Mamag0 <william.desgagne@hotmail.com>
2025-09-19 14:02:35 -0400

    first commit

---
+++ src/main/java/com/log430/brockerx/BrockerXApplication.java
@@ -0,0 +1,13 @@
+package com.log430.brockerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrockerXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrockerXApplication.class, args);
+    }
+
+}
