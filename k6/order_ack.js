51c8bbbc52c8523b7613a53ee57c63b056dead37
Mamag0 <william.desgagne@hotmail.com>
2025-10-11 20:11:19 -0400

    change test k6

--- k6/order_ack.js
+++ k6/order_ack.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export let options = {
-    vus: 200,              // un seul thread (il exécute en boucle les échanges)
+    vus: 10,              // un seul thread (il exécute en boucle les échanges)
     duration: '300s',    // 5 minutes
 };
 
